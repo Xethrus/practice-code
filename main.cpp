6113ce56dd1717f10cdeacf4191beb536c2afc76
xethrus <xethrus@gmail.com>
2022-12-04 18:58:52 -0800

    working logic

--- main.cpp
+++ main.cpp
@@ -5,29 +5,16 @@ int main() {
   int divisor = 10;
   int number = 975;
   int temp = number;
-  int ref = temp;
-  int counter = 0;
-  int result = 0;
+  int count = 0;
+  int printVal;
   while (number / divisor > 0) {
     number = number / divisor;
-    counter++;
+    count++;
   }
-  for(int i = 0; i <= counter; i++) {
-    int count = 0;
-    while (temp / divisor > 0) {
-      temp = temp / divisor;
-      count++;
-    } 
-    std::cout << "temp: " << temp << std::endl;
-    temp = ref - (std::pow(divisor, count) * temp);
-    std::cout << "temp after: " << temp << std::endl;
-    //temp = 975 - 900;
-    //ref = 975 - 900;
-    //temp = 75 - 70;
-    //ref = 5;
-    //temp = 5 - 0
-    ref = ref - (std::pow(divisor, count) * temp);
-
+  int array[count + 1] = {};
+  for (int i = 1; i <= count + 1; i++) {
+    int power = std::pow(divisor, i);
+    std::cout << (temp % power) << std::endl;
+    array[i] = (temp % power);
   }
 }
-
