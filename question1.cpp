747db59203d9f5c3e06baffc1aee77ccd5a8c28c
xethrus <xethrus@gmail.com>
2022-12-06 16:01:55 -0800

    basically works

--- question1.cpp
+++ question1.cpp
@@ -12,17 +12,33 @@ public:
 class Stack {
 public:
   Node* top;
-
+  int arraySize = 9;
+  int array[9] = {1,2,3,4,5,6,7,8,9};
   Node* pop();
   void push(int);
   void peek();
+  void fillThreeStacks();
+  void printStack();
 };
 
 Node* Stack::pop() {
   if (top == NULL) {
     return NULL;
   }
-  return top;
+  Node* temp = top;
+  top = top->below;
+  return temp;
+}
+void Stack::printStack() {
+  while (true) {
+    Node* temp = pop();
+    std::cout << "temp: " << temp << std::endl;
+    if (temp == NULL) {
+      return;
+    }
+    std::cout << temp->data << std::endl; 
+  }
+  return;
 }
 
 void Stack::push(int data) {
@@ -35,16 +51,20 @@ void Stack::push(int data) {
   }
   return;
 }
-
+void Stack::fillThreeStacks() {
+  Stack stack1;
+  Stack stack2;
+  Stack stack3;
+  for (int i = 0; i < arraySize; i += 3) {
+    stack1.push(array[i]);
+    stack2.push(array[i+1]);
+    stack3.push(array[i+2]);
+  }
+  stack1.printStack();
+  stack2.printStack();
+}
 int main() {
   Stack stack;
-  stack.push(1);
-  stack.push(1);
-  stack.push(1);
-  stack.push(1);
-  stack.push(1);
-  stack.push(1);
-  stack.push(1);
-  stack.push(1);
+  stack.fillThreeStacks();
   return 0;
 }
